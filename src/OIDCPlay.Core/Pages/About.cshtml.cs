46d5484ddfbd7fe43fd2cbf63e876987bfb1e92a
Herb Stahl <ghstahl@gmail.com>
2018-09-27 15:24:47 -0700

    decoding jwt if we have it.

--- src/OIDCPlay.Core/Pages/About.cshtml.cs
+++ src/OIDCPlay.Core/Pages/About.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using OIDCPlay.Core.Areas.Identity.Pages.Account;
 
 namespace OIDCPlay.Core.Pages
@@ -21,6 +24,44 @@ namespace OIDCPlay.Core.Pages
         }
         public string Message { get; set; }
 
+        private string DecodeJwt(string jwtToken)
+        {
+            var jwtHandler = new JwtSecurityTokenHandler();
+            //Check if readable token (string is in a JWT format)
+            var readableToken = jwtHandler.CanReadToken(jwtToken);
+            if (readableToken != true)
+            {
+                return null;
+            }
+            else
+            {
+                var token = jwtHandler.ReadJwtToken(jwtToken);
+
+                //Extract the headers of the JWT
+                var headers = token.Header;
+                var jwtHeader = "{";
+                foreach (var h in headers)
+                {
+                    jwtHeader += '"' + h.Key + "\":\"" + h.Value + "\",";
+                }
+                jwtHeader += "}";
+                var jwtIdToken = "Header:\r\n" + JToken.Parse(jwtHeader).ToString(Formatting.Indented);
+
+                //Extract the payload of the JWT
+                var claims = token.Claims;
+                var jwtPayload = "{";
+                foreach (Claim c in claims)
+                {
+                    jwtPayload += '"' + c.Type + "\":\"" + c.Value + "\",";
+                }
+                jwtPayload += "}";
+                jwtIdToken += "\r\nPayload:\r\n" + JToken.Parse(jwtPayload).ToString(Formatting.Indented);
+
+
+                return jwtIdToken;
+            }
+
+        }
         public void OnGet()
         {
             Message = "Your application description page.";
@@ -32,6 +73,20 @@ namespace OIDCPlay.Core.Pages
                     // unprotect the payload
                     string unprotectedPayload = _protector.Unprotect(protectedPayload);
                     OIDC = JsonConvert.DeserializeObject<Dictionary<string,string>>(unprotectedPayload);
+                    var decodedDictionary = new Dictionary<string,string>();
+                    foreach (var item in OIDC)
+                    {
+                        var decodedItem = DecodeJwt(item.Value);
+                        if (!string.IsNullOrEmpty(decodedItem))
+                        {
+                            decodedDictionary.Add($"{item.Key}.decoded", decodedItem);
+                        }
+                    }
+
+                    foreach (var item in decodedDictionary)
+                    {
+                        OIDC.Add(item.Key,item.Value);
+                    }
                 }
             }
         }
